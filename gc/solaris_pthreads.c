715893e436f3cc75932cf00d3324bd1fd5b6d4b3
schani <schani@cefd48d6-89e4-0310-8b86-be731dd2b999>
1998-05-04 21:16:55 +0000

    Updated gc to 4.13alpha1.
    Fixed a typo in ANNOUNCEMENT.
    
    
    git-svn-id: http://darwin/svn/unix/chpp/trunk@118 cefd48d6-89e4-0310-8b86-be731dd2b999

--- gc/solaris_pthreads.c
+++ gc/solaris_pthreads.c
@@ -16,7 +16,7 @@
  * Modified Peter C. for Solaris Posix Threads.
  */
 /* Boehm, September 14, 1994 4:44 pm PDT */
-/* $Id: solaris_pthreads.c,v 1.1 1998/04/30 14:56:54 cvs Exp $ */
+/* $Id: solaris_pthreads.c,v 1.2 1998/05/04 21:16:54 schani Exp $ */
 
 # if defined(_SOLARIS_PTHREADS)
 # include "gc_priv.h"
@@ -36,6 +36,7 @@
 # define _CLASSIC_XOPEN_TYPES
 # include <unistd.h>
 # include <errno.h>
+# include "solaris_threads.h"
 
 #undef pthread_join
 #undef pthread_create
@@ -163,7 +164,7 @@ GC_pthread_create(pthread_t *new_thread,
 # else
 
 #ifndef LINT
-  int GC_no_sunOS_threads;
+  int GC_no_sunOS_pthreads;
 #endif
 
 # endif /* SOLARIS_THREADS */
